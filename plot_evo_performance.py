d852355a07f3794810be76a9176eee4a27018cc6
Calvin Pelletier <calvin.pelletier@gmail.com>
2018-05-03 16:06:07 -0500

    added discriminator weights and onehot data

---
+++ plot_evo_performance.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import os
+
+x = []
+y = []
+for filename in os.listdir('results'):
+    with open('results/' + filename, 'r') as f:
+        lines = f.readlines()
+        for i in range(len(lines)):
+            if lines[i].startswith('BEST OF GEN'):
+                if i < len(lines) - 1:
+                    fitness = float(lines[i+1].split('=')[1].split('(')[0].strip())
+                    if fitness > 2.7:
+                        overhead = float(lines[i].split(',')[0].split('=')[1].strip())
+                        acc = float(lines[i].split(',')[1].split('=')[1].strip())
+                        x.append(overhead)
+                        y.append(acc)
+
+base_x = []
+base_y = []
+with open('random_insertion_results.txt', 'r') as f:
+    for line in f:
+        overhead = float(line.split(',')[0].split('=')[1].strip())
+        acc = float(line.split(',')[1].split('=')[1].strip())
+        base_x.append(overhead)
+        base_y.append(acc)
+
+plt.plot(x, y, 'bs', base_x, base_y, 'r--')
+plt.savefig('evo_performance.png')
