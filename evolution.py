ab4e1c005c570a33e238d3415b019c32ca62ca53
Calvin Pelletier <calvin.pelletier@gmail.com>
2018-05-03 12:10:28 -0500

    .

--- evolution.py
+++ evolution.py
@@ -7,6 +7,7 @@ from dlwf.kerasdlwf import tor_cnn
 from configobj import ConfigObj
 import time
 import sys
+import random
 
 INPUT_SIZE = 10 + 1 # add 1 for bias
 HIDDEN_SIZE = 100
@@ -15,11 +16,15 @@ N_GENS = 10000
 SEQ_LEN = 3000
 POP_SIZE = 100
 HALF_MUTATE_RANGE = 0.1
+MIN_HALF_MUTATE_RANGE = 0.01
+SHRINK_MUT_RANGE_OVER_N_GENS = 9
+MUT_STEP = (HALF_MUTATE_RANGE - MIN_HALF_MUTATE_RANGE) / float(SHRINK_MUT_RANGE_OVER_N_GENS)
 DISCRIMINATOR_PATH = 'dlwf/kerasdlwf/models/2904_181830_cnn'
 OVERHEAD_FITNESS_MULTIPLIER = 2.
 SAMPLES_PER_GEN = 500
 LOAD_DISCRIMINATOR = False
 INDEX = '0'
+LSTM_UNITS = 10
 
 # class MLP:
 #     def __init__(self):
@@ -32,7 +37,7 @@ INDEX = '0'
 
 def create_model():
     model = k.models.Sequential()
-    model.add(k.layers.LSTM(10, batch_input_shape=(SAMPLES_PER_GEN, SEQ_LEN, 1), return_sequences=True))
+    model.add(k.layers.LSTM(LSTM_UNITS, batch_input_shape=(SAMPLES_PER_GEN, SEQ_LEN, 1), return_sequences=True))
     model.add(k.layers.Dense(3))
     return model
 
@@ -47,18 +52,18 @@ def mutate(model):
     return new_model
 
 
-def mutate_fast(model):
-    new_model = create_model()
-    weights_list = model.get_weights()
-    for weights in weights_list:
-        mut = np.random.uniform(-HALF_MUTATE_RANGE, HALF_MUTATE_RANGE, weights.shape)
-        weights += mut
-    new_model.set_weights(weights_list)
-    return new_model
+def mutate_fast(population, best):
+    best_weights = population[best].get_weights()
+    for i in range(POP_SIZE):
+        new_weights = []
+        for weights in best_weights:
+            mut = np.random.uniform(-HALF_MUTATE_RANGE, HALF_MUTATE_RANGE, weights.shape)
+            new_weights.append(weights + mut)
+        population[i].set_weights(new_weights)
+    return population
 
 
 def insert_data(generator_output, original_data):
-    # return original_data, 0
     new_data = np.empty(original_data.shape)
     generator_output = np.argmax(generator_output, axis=2)
     insertions = 0
@@ -82,6 +87,51 @@ def insert_data(generator_output, original_data):
                 x += 1
                 insertions += 1
     return new_data, insertions
+
+
+def insert_data_randomly(original_data, percent):
+    new_data = np.empty(original_data.shape)
+    insertions = 0
+    for i in range(original_data.shape[0]):
+        x = 0
+        for j in range(original_data.shape[1]):
+            if x == original_data.shape[1]:
+                break
+            new_data[i][x] = original_data[i][j]
+            x += 1
+            if x == original_data.shape[1]:
+                break
+            if random.random() < percent:
+                if random.random() < 0.5:
+                    # insert 1
+                    new_data[i][x] = [1.]
+                    x += 1
+                    insertions += 1
+                else:
+                    # insert -1
+                    new_data[i][x] = [-1.]
+                    x += 1
+                    insertions += 1
+    return new_data, insertions
+
+
+def eval_random_insertion(discriminator):
+    f = open('random_insertion_results.txt', 'w')
+    test_data, test_labels = load_data('data/test.npz', maxlen=SEQ_LEN, traces=1500, dnn_type='cnn')
+    N_TEST_SAMPLES = test_data.shape[0]
+    base_accuracy = eval(discriminator, test_data, test_labels, 256)
+    # base_accuracy = discriminator.evaluate(test_data, test_labels, batch_size=256)[1]
+    print('BASE TEST ACC: {}'.format(base_accuracy))
+
+    for p in range(1, 100):
+        percent = float(p) / float(100)
+        new_data, insertions = insert_data_randomly(test_data, percent)
+        acc = discriminator.evaluate(new_data, test_labels, batch_size=SAMPLES_PER_GEN)[1]
+        overhead = float(insertions) / float(N_TEST_SAMPLES * SEQ_LEN)
+        output = 'RANDOM INSERTION CHANCE {}: test overhead = {:.4f}, test acc = {:.4f}'.format(percent, overhead, acc)
+        print(output)
+        f.write(output + '\n')
+    f.close()
 
 
 def train(datapath):
@@ -224,7 +274,9 @@ def run(discriminator):
 
 
     # create initial population
-    population = [create_model()]
+    population = []
+    for _ in range(POP_SIZE):
+        population.append(create_model())
 
     # generational loop
     for gen in range(N_GENS):
@@ -264,13 +316,14 @@ def run(discriminator):
         population[best].save_weights('best_generator_weights{}.h5'.format(INDEX))
 
         # mutate
-        new_pop = []
-        for _ in range(POP_SIZE):
-            new_pop.append(mutate_fast(population[best]))
-        population = new_pop
+        # new_pop = []
+        # for _ in range(POP_SIZE):
+        #     new_pop.append(mutate_fast(population[best]))
+        # population = new_pop
+        new_pop = mutate_fast(population, best)
 
         # shrink mutation range
-        HALF_MUTATE_RANGE = max(0.01, HALF_MUTATE_RANGE - 0.01)
+        HALF_MUTATE_RANGE = max(MIN_HALF_MUTATE_RANGE, HALF_MUTATE_RANGE - MUT_STEP)
 
         end = time.time()
         output = 'GEN {}: best fitness = {:.4f} (took {:.2f} sec)'.format(gen, fitness[best], end-start)
@@ -282,5 +335,19 @@ def run(discriminator):
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         INDEX = sys.argv[1]
+        print('index: {}'.format(INDEX))
+    idx = int(INDEX)
+    if idx == 1 or idx == 2:
+        POP_SIZE = 200
+    elif idx == 3 or idx == 4:
+        LSTM_UNITS = 20
+    elif idx == 5:
+        MIN_HALF_MUTATE_RANGE = 0.02
+    elif idx == 6:
+        HALF_MUTATE_RANGE = 0.2
+    elif idx == 7:
+        POP_SIZE = 200
+        LSTM_UNITS = 20
     discriminator = train('data/train.npz')
     run(discriminator)
+    # eval_random_insertion(discriminator)
